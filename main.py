b402101cd1c494d26a1391bce707e1b6a51f124a
ETTORE PADULA DALBEN <20.00387-0@maua.br>
2023-05-23 17:16:18 -0300

    Código melhor

--- main.py
+++ main.py
@@ -15,60 +15,58 @@ TRIGGER_PIN = 27
 trigger = Pin(TRIGGER_PIN, Pin.OUT)
 echo = Pin(ECHO_PIN, Pin.IN)
 
-
-
 led = Pin(25, Pin.OUT)
 
-# motor_a ports
-ina1 = Pin(11, Pin.OUT)
-ina2 = Pin(10, Pin.OUT)
-pwm_a = PWM(Pin(12))
+# motor_a (esquerda) ports
+ina1 = Pin(11, Pin.OUT) # Verde
+ina2 = Pin(10, Pin.OUT) # Marrom
+#pwm_a = PWM(Pin(12)) 
 
-# motor_b ports
-inb1 = Pin(15, Pin.OUT)
-inb2 = Pin(13, Pin.OUT)
-pwm_b = PWM(Pin(14))
+# motor_b (direita) ports
+inb1 = Pin(15, Pin.OUT) # Lilás
+inb2 = Pin(14, Pin.OUT) # Branco
+#pwm_b = PWM(Pin(13))
 
-pwm_a.freq(10000)
-pwm_b.freq(10000)
-
-def left_motor_forward(duty):
-    inb1.value(0)
-    inb2.value(1)
-    duty_16 = int((duty * 65536) / 100)
-    #print("motor esquerdo", duty_16)
-    pwm_b.duty_u16(duty_16)
-    
-def left_motor_backward(duty):
-    inb1.value(1)
-    led.value(1)
-    inb2.value(0)
-    duty_16 = int((duty * 65536) / 100)
-    pwm_b.duty_u16(duty_16)
-
-def stop_left_motor():
-    pwm_b.duty_u16(0)
-    inb2.value(0)
-    inb1.value(0)
+#pwm_a.freq(10000)
+#pwm_b.freq(10000)
 
 def right_motor_forward(duty):
+    inb1.value(0)
+    inb2.value(1)
+    #duty_16 = int((duty * 65536) / 100)
+    #print("motor esquerdo", duty_16)
+    #pwm_b.duty_u16(duty_16)
+    
+def right_motor_backward(duty):
+    inb1.value(1)
+    inb2.value(0)
+    led.value(1)
+    #duty_16 = int((duty * 65536) / 100)
+    #pwm_b.duty_u16(duty_16)
+
+def stop_right_motor():
+    inb2.value(0)
+    inb1.value(0)
+    #pwm_b.duty_u16(0)
+
+def left_motor_forward(duty):
     ina1.value(0)
     ina2.value(1)
     led.value(0)
-    duty_16 = int((duty * 65536) / 100)
+    #duty_16 = int((duty * 65536) / 100)
     #print("motor direito", duty_16)
-    pwm_a.duty_u16(duty_16)
+    #pwm_a.duty_u16(duty_16)
 
-def right_motor_backward(duty):
+def left_motor_backward(duty):
     ina1.value(1)
     ina2.value(0)
-    duty_16 = int((duty * 65536) // 100)
-    pwm_a.duty_u16(duty_16)
+    #duty_16 = int((duty * 65536) / 100)
+    #pwm_a.duty_u16(duty_16)
 
-def stop_right_motor():
-    inb1.value(0)
-    inb2.value(0)
-    pwm_a.duty_u16(0)
+def stop_left_motor():
+    ina1.value(0)
+    ina2.value(0)
+    #pwm_a.duty_u16(0)
     
 # Forward
 def move_forward(duty):    
@@ -77,16 +75,16 @@ def move_forward(duty):
     
 # Backward
 def move_backward(duty):
-    left_motor_backward(duty)
     right_motor_backward(duty)
+    left_motor_backward(duty)
     
 #Turn Left
-def turn_left(duty):
+def turn_right(duty):
     right_motor_backward(duty)
     left_motor_forward(duty)
     
 #Turn Right
-def turn_right(duty):
+def turn_left(duty):
     left_motor_backward(duty)
     right_motor_forward(duty)
    
@@ -108,7 +106,6 @@ def test_right(duty):
     sleep(1)
 
 def getDistance(ECHO_PIN,TRIGGER_PIN):
-    
     
     trigger = Pin(TRIGGER_PIN, Pin.OUT)
     echo = Pin(ECHO_PIN, Pin.IN)
@@ -132,28 +129,24 @@ stopDistance=50
 
 # sleep(10)
 def test(M):
-    print(0)
-    test_left(M)
-    stop()
-    sleep(1)
-    test_right(M)
-    stop()
-    sleep(1)
-    
     move_forward(M)
+    sleep(1)
+    stop()
     sleep(1)
     move_backward(M)
     sleep(1)
-    turn_right(M)
+    stop()
     sleep(1)
     turn_left(M)
     sleep(1)
     stop()
+    sleep(1)
+    turn_right(M)
     
 if True:
     test(M)
 stopDist=50    
-while True:
+while False:
 
     distance_mid = getDistance(7,6)
     
