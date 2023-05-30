8395a3178e05bb76e287b79b556bc207225a690f
GUILHERME DE CAMPOS MELLO GUERRA MARTINS <20.00089-8@maua.br>
2023-05-30 19:36:11 -0300

    codigo funcional e clean

--- main.py
+++ main.py
@@ -4,132 +4,104 @@ from machine import time_pulse_us
 from time import sleep_us, sleep
 import random
 import utime
-M=100
-
 
 # Define the GPIO pin numbers for the trigger and echo pins
-ECHO_PIN = 2
-ECHO1 = 3
-ECHO2 = 4
-TRIGGER_PIN =1 
-
+TRIGGER_PIN = 1 
+ECHO_LEFT = 4
+ECHO_MIDDLE = 3
+ECHO_RIGHT = 2
 # Initialize trigger and echo pins
-trigger = Pin(TRIGGER_PIN, Pin.OUT)
-echo = Pin(ECHO_PIN, Pin.IN)
-
-led = Pin(25, Pin.OUT)
 
 # motor_a (esquerda) ports
 ina1 = Pin(11, Pin.OUT) # Verde
 ina2 = Pin(10, Pin.OUT) # Marrom
-#pwm_a = PWM(Pin(12)) 
 
 # motor_b (direita) ports
 inb1 = Pin(15, Pin.OUT) # Lilás
 inb2 = Pin(14, Pin.OUT) # Branco
-#pwm_b = PWM(Pin(13))
 
-#pwm_a.freq(10000)
-#pwm_b.freq(10000)
-
-def right_motor_forward(duty):
+def right_motor_forward():
     inb1.value(0)
     inb2.value(1)
-    #duty_16 = int((duty * 65536) / 100)
-    #print("motor esquerdo", duty_16)
-    #pwm_b.duty_u16(duty_16)
     
-def right_motor_backward(duty):
+def right_motor_backward():
     inb1.value(1)
     inb2.value(0)
-    led.value(1)
-    #duty_16 = int((duty * 65536) / 100)
-    #pwm_b.duty_u16(duty_16)
 
 def stop_right_motor():
     inb2.value(0)
     inb1.value(0)
-    #pwm_b.duty_u16(0)
 
-def left_motor_forward(duty):
+def left_motor_forward():
     ina1.value(0)
     ina2.value(1)
-    led.value(0)
-    #duty_16 = int((duty * 65536) / 100)
-    #print("motor direito", duty_16)
-    #pwm_a.duty_u16(duty_16)
 
-def left_motor_backward(duty):
+def left_motor_backward():
     ina1.value(1)
     ina2.value(0)
-    #duty_16 = int((duty * 65536) / 100)
-    #pwm_a.duty_u16(duty_16)
 
 def stop_left_motor():
     ina1.value(0)
     ina2.value(0)
-    #pwm_a.duty_u16(0)
     
 # Forward
-def move_forward(duty):    
-    right_motor_forward(duty)
-    left_motor_forward(duty)
+def move_forward():    
+    right_motor_forward()
+    left_motor_forward()
     
 # Backward
-def move_backward(duty):
-    right_motor_backward(duty)
-    left_motor_backward(duty)
+def move_backward():
+    right_motor_backward()
+    left_motor_backward()
     
 #Turn Left
-def turn_right(duty):
-    right_motor_backward(duty)
-    left_motor_forward(duty)
+def turn_right():
+    right_motor_backward()
+    left_motor_forward()
     
 #Turn Right
-def turn_left(duty):
-    left_motor_backward(duty)
-    right_motor_forward(duty)
+def turn_left():
+    left_motor_backward()
+    right_motor_forward()
    
 #Stop
 def stop():
     stop_right_motor()
     stop_left_motor()
 
-def test_left(duty):
-    left_motor_forward(duty)
+def test_left():
+    left_motor_forward()
     sleep(1)
-    left_motor_backward(duty)
+    left_motor_backward()
     sleep(1)
     
-def test_right(duty):
-    right_motor_forward(duty)
+def test_right():
+    right_motor_forward()
     sleep(1)
-    right_motor_backward(duty)
+    right_motor_backward()
     sleep(1)
 
-def getDistance(echo_pin,trigger_pin):
+def getDistance(echo_pinL, echo_pinM, echo_pinR,trigger_pin):
     
     trigger = Pin(trigger_pin, Pin.OUT)
-    echo = Pin(echo_pin, Pin.IN)
+    
+    echos = (Pin(echo_pinL, Pin.IN), Pin(echo_pinM, Pin.IN), Pin(echo_pinR, Pin.IN))
     # Ensure trigger is low initially
     trigger.low()
     sleep_us(200)
 
     # Send a 10 microsecond pulse to the trigger pin
     trigger.high()
-    sleep_us(100)
+    sleep_us(10)
     trigger.low()
-
-    # Measure the duration of the echo pulse (in microseconds)
-    pulse_duration = time_pulse_us(echo, Pin.high)
-
-    # Calculate the distance (in centimeters) using the speed of sound (343 m/s)
-    distance = pulse_duration * 0.0343 / 2
-    return distance
+    
+    sound_speed = 0.0343
+    distances = (time_pulse_us(echo, Pin.high) for echo in echos)
+    
+    return (dist * sound_speed / 2 for dist in distances)
 
 stopDistance=50
 
-# sleep(10)
 def test(M):
     move_forward(M)
     sleep(1)
@@ -145,14 +117,10 @@ def test(M):
     sleep(1)
     turn_right(M)
     
-if True:
-    test(M)
-    
-stopDist=50    
+stopDist=50
 while True:
-    distance_mid = getDistance(2,1)
-    distance_left = getDistance(3,1)
-    distance_right = getDistance(4,1)
-    print(f"2     {distance_mid}")
-    print(f"3     {distance_left}")
-    print(f"4     {distance_right}")
+    sleep(0.1)
+    distance_left, distance_mid, distance_right = getDistance(ECHO_LEFT, ECHO_MIDDLE, ECHO_RIGHT, TRIGGER_PIN)
+
+    print("%.2f \t %.2f \t %.2f" %(distance_left, distance_mid, distance_right))
+    
